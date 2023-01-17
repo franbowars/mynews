8b14db127ca937af8e679a90ddfc91070ffdc177
y-adachi <yfe27665@gmail.com>
2023-01-17 06:13:17 +0000

    PHP17 kada

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\News;
-
+use App\Models\History;
+use Carbon\Carbon;
 class NewsController extends Controller
 {
     public function add(){
@@ -83,6 +84,12 @@ class NewsController extends Controller
         
         // 該当するデータを上書きして保存する
         $news->fill($news_form)->save();
+        
+        $history = new History();
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
         
         return redirect('admin/news');
     }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Profile;
+use App\Models\ProfileHistory;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -49,9 +51,15 @@ class ProfileController extends Controller
         
         $profile->fill($profile_form)->save();
         
+        $profile_history = new ProfileHistory();
+        $profile_history->profile_id = $profile->id;
+        $profile_history->edited_at = Carbon::now();
+        $profile_history->save();
+        
         return redirect('admin/profile/edit?id=' . $profile->id);
         
     }
+    
 }
 
 
